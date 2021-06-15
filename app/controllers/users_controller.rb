17da28cde3577018d813c1f52f19752266cfa623
yuki.kirakirara <da1am0nd0.ll.tt@gmail.com>
2021-06-15 03:09:11 +0000

    favorites_microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
 
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
+    
   end
 
   def show
      @user = User.find(params[:id])
      @microposts = @user.microposts.order(id: :desc).page(params[:page])
      counts(@user)
-     @favorites = @user. favorites.page(params[:page])
      
   end
 
@@ -41,12 +41,12 @@ class UsersController < ApplicationController
   end
   
   def likes
-    @user = User.find(params[:id])
-    @favorites = @user.favorites.page(params[:page])
-    counts(@user)
-    
+     @user = User.find(params[:id])
+     @favorites = @user.favorites.page(params[:page])
+     @micropost = @user.favorite_microposts.page(params[:page])
+     counts(@user)
   end
-  
+    
   
    private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :followers, through: :reverses_of_relationship, source: :user
   
   has_many :favorites, dependent: :destroy
-  has_many :likes,through: :favorites, source: :micropost
+  has_many :favorite_microposts,through: :favorites, source: :micropost
   
    def follow(other_user)
     unless self == other_user
@@ -31,17 +31,17 @@ class User < ApplicationRecord
   end
   
   
-    def favorite(micropost)
-      self.favorites.find_or_create_by(micropost_id: micropost.id)
+    def favorite(other_micropost)
+      self.favorites.find_or_create_by(micropost_id: other_micropost.id)
     end
   
-    def unfavorite(micropost)
-      favorite = self.favorites.find_by(micropost_id: micropost.id)
+    def unfavorite(other_micropost)
+      favorite = self.favorites.find_by(micropost_id: other_micropost.id)
       favorite.destroy if favorite
     end
     
-    def likes?(micropost)
-      self.likes.include?(micropost)
+    def favorite?(other_micropost)
+      self.favorite_microposts.include?(other_micropost)
     end
   
   def feed_microposts
